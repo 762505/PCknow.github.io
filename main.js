80db29bec6237e2d73d5431ad3a549805af69fbe
pceveryknow <913945903@qq.com>
2021-11-29 21:10:24 +0800

    added new files

---
+++ main.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+  var Image1 = document.createElement("img");    
+  Image1.src = "cpu.png";      
+  Image1.width = 300;    
+  Image1.height = 180;    
+  document.body.appendChild(Image1); 
+ </script>
